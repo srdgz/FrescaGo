aa5c6fb81ec06dc8df09d31de48d94b288ca9fdd
srdgz <sandra.rr@icloud.com>
2024-08-11 12:57:40 +0200

    data extracted to a new file

---
+++ frescaGo/src/data/data.js
@@ -0,0 +1,34 @@
+export const GeneralProductsData = [
+  {
+    id: 1,
+    title: "Manzanas variadas",
+    link: "#",
+    price: "desde 2,35€/kg",
+    delay: 0.3,
+    img: "https://www.compraonline.alcampo.es/images-v3/37ea0506-72ec-4543-93c8-a77bb916ec12/330ae5b8-0960-432a-b850-3e906fabdaeb/200x200.webp",
+  },
+  {
+    id: 2,
+    title: "Plátano y bananas",
+    link: "#",
+    price: "desde 1,89€/kg",
+    delay: 0.6,
+    img: "https://www.compraonline.alcampo.es/images-v3/37ea0506-72ec-4543-93c8-a77bb916ec12/d31e072b-3526-4f0d-b09d-cbc4c47a218a/200x200.webp",
+  },
+  {
+    id: 3,
+    title: "Verduras de hoja verde",
+    link: "#",
+    price: "desde 0,99€/kg",
+    delay: 0.9,
+    img: "https://www.compraonline.alcampo.es/images-v3/37ea0506-72ec-4543-93c8-a77bb916ec12/ac1421ab-988d-4aa4-8470-dac08dd2904c/1280x1280.webp",
+  },
+  {
+    id: 4,
+    title: "Tomates variados",
+    link: "#",
+    price: "desde 1,69€/kg",
+    delay: 1.2,
+    img: "https://www.compraonline.alcampo.es/images-v3/37ea0506-72ec-4543-93c8-a77bb916ec12/f2857bf9-b4e5-4dba-b50c-2da523b715d1/1280x1280.webp",
+  },
+];
