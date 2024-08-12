5f460748a8f522e7168a9df0c0c9967c162109a1
srdgz <sandra.rr@icloud.com>
2024-08-12 13:19:53 +0200

    adding link to logo

--- frescaGo/src/components/Utils/Logo.jsx
+++ frescaGo/src/components/Utils/Logo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { RiLeafFill } from "react-icons/ri";
 
 const Logo = () => {
   return (
-    <div className="text-2xl flex items-center gap-2 font-bold">
+    <Link to="/" className="text-2xl flex items-center gap-2 font-bold">
       <p className="text-primary">
         Fresca<span className="text-secondary">Go</span>
       </p>
       <RiLeafFill className="text-green-500" />
-    </div>
+    </Link>
   );
 };
 
